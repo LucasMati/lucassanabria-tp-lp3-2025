e59acdbe34ab8cfec500c3db75981ea8056e8027
LucasMati <lucassanabriac@gmail.com>
2025-09-12 11:25:28 -0300

    Entrega TP Spring Boot

---
+++ lp32025/src/main/java/py/edu/uc/lp32025/controller/IndexController.java
@@ -0,0 +1,29 @@
+package py.edu.uc.lp32025.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import py.edu.uc.lp32025.dto.SaludoDto;
+
+@RestController
+public class IndexController {
+
+    @RequestMapping("/")
+    public String redirectToHolaMundo() {
+        return "redirect:/HolaMundo";
+    }
+
+    @RequestMapping("/HolaMundo")
+    public SaludoDto HolaMundo() {
+        return new SaludoDto("¡Hola desde Spring Boot!");
+    }
+
+    @GetMapping("/HolaMundo/{nombre}")
+    public SaludoDto holaMundo(@PathVariable String nombre) {
+        if (nombre.length() < 2) {
+            return new SaludoDto(400, "Nombre demasiado corto", "Por favor, introduce un nombre más largo.");
+        }
+        return new SaludoDto("¡Hola " + nombre + " desde Spring Boot!");
+    }
+}
